eb88ed325266cf28131e48bce3d7c8b8e67baf8e
lsfarm <58246081+lsfarm@users.noreply.github.com>
2021-02-10 18:24:28 -0600

    Update greenhousemonitor.ino

--- greenhousemonitor.ino
+++ greenhousemonitor.ino
@@ -1,3 +1,8 @@
+// This #include statement was automatically added by the Particle IDE.
+//#include <PublishQueueAsyncRK.h>  //https://github.com/rickkas7/PublishQueueAsyncRK
+//retained uint8_t publishQueueRetainedBuffer[2048];
+//PublishQueueAsync publishQueue(publishQueueRetainedBuffer, sizeof(publishQueueRetainedBuffer));
+
 /////////************* **********/////////
 //          Blynk Assignments           //
 /////////************* **********/////////
@@ -17,7 +22,7 @@ SYSTEM_THREAD(ENABLED);
 #define Location LS  //location of device
 #define db2p TRUE  //debug2particle   >>if(db2p) {Particle.publish("in db2p");}
 //////******Still publishing to particle in createEventPayload!!!!<<boron data usage
-#define SENSOR_READING_INTERVAL 30000  //<<< in loop()  sendAlert() relies on this
+#define SENSOR_READING_INTERVAL 30  //<<< in loop()  sendAlert() relies on this 30 = 3 sec.
 #define FirstRunDelay 8000
 bool firstrunlockout = 1;
 long sendinfo2blynk = 60000;
@@ -28,7 +33,12 @@ bool sendtoParticle = 0;
 /////////************* **********/////////
 //                Variables             //
 /////////************* **********/////////
+bool BME280Status;
+bool SCD30Status;
+//bool BME280Status;
 #include <blynk.h>
+bool ReCnctFlag = 0;
+int ReCnctCount;
 WidgetTerminal terminal(V5);
 //char auth[] = "Mi23GHp-MVumIsjfunxSQT_WxnutVvs1";  //Red Barn Greenhouse in Blynk
 //#if ("LS")
@@ -94,57 +104,35 @@ BLYNK_WRITE(V20) {
   alertenable = param.asInt(); // assigning incoming value from pin to a variable
   digitalWrite(BlynkAlertLED, alertenable);
 }
-  
-void setup() {
-  delay(50);
-  Time.zone(-6);  //-5for old time
-  Blynk.begin(auth);
-  timer.setInterval(sendinfo2blynk, sendinfo);
-  timer.setInterval(sendalert2blynk, sendAlert);
-  
-  EEPROM.get(1, lowalertsetpoint);
-  EEPROM.get(50, highalertsetpoint);
-  Blynk.virtualWrite(V20, alertenable);
-  Blynk.virtualWrite(V10, lowalertsetpoint);
-  Blynk.virtualWrite(V11, highalertsetpoint);
-  Blynk.notify(String("Device Rebooting... Allow 1 min for sensor readings to settle")); // +  myStr + ("°F"));
-  pinMode(BlynkAlertLED, OUTPUT);
 
+void sensorInit() {
   // Configure the dust sensor pin as an input
   pinMode(DUST_SENSOR_PIN, INPUT);
   if (aqSensor.init()) {
-    if(db2p && Particle.connected()) Particle.publish("DustSen", "Ready", PRIVATE | WITH_ACK);
+      //publishQueue.publish("DustSen", "Ready", PRIVATE | WITH_ACK);
+    if(db2p) Particle.publish("DustSen", "Ready", PRIVATE | WITH_ACK); delay(1000); //if(db2p && Particle.connected()) {bool result = Particle.publish("DustSen", "Ready", PRIVATE | WITH_ACK); delay(4000);}
   }
   else {
     if(db2p) Particle.publish("DustSen", "Error"); delay(1000);
   }
-
-  Wire.begin();
-  SeeedOled.init();
-
-  SeeedOled.clearDisplay();
-  SeeedOled.setNormalDisplay();
-  SeeedOled.setPageMode();
-
-  SeeedOled.setTextXY(2, 0);
-  SeeedOled.putString("Particle");
-  SeeedOled.setTextXY(3, 0);
-  SeeedOled.putString("Air Quality");
-  SeeedOled.setTextXY(4, 0);
-  SeeedOled.putString("Monitor");
-
-  if (bme.begin()) {
+  if (bme.begin()) { //BME280
+      //publishQueue.publish("BME280", "Ready", PRIVATE | WITH_ACK);
     if(db2p) Particle.publish("BME280", "Ready", PRIVATE | WITH_ACK); delay(1000);
+    BME280Status = 1;
   }
   else {
     if(db2p) Particle.publish("BME280", "Error"); delay(1000);
+    BME280Status = 0;
   }
   
-  if (airSensor.begin()) {
-    if(db2p) Particle.publish("SCD30", "Ready"); delay(1000);
+  if (airSensor.begin()) { //SCD30
+      //publishQueue.publish("SCD30", "Ready", PRIVATE | WITH_ACK);
+    if(db2p) Particle.publish("SCD30", "Ready"); //delay(5000);
+    SCD30Status = 1;
   }
   else {
     if(db2p) Particle.publish("SCD30", "Error"); delay(1000);
+    SCD30Status = 0;
   }
   airSensor.setMeasurementInterval(8); //Change number of seconds between measurements: 2 to 1800 (30 minutes)
   //My desk is ~1600m above sealevel
@@ -152,51 +140,77 @@ void setup() {
   //Pressure in Boulder, CO is 24.65inHg or 834.74mBar
   //airSensor.setAmbientPressure(835); //Current ambient pressure in mBar: 700 to 1200
 
-  lastInterval = millis();
+
+}
+  
+void setup() {
+    //delay(5000);
+    //sensorInit();
+    delay(50);
+    Time.zone(-6);  //-5for old time
+    Blynk.begin(auth);
+    //sensorInit();
+    timer.setInterval(30000L, updateAllSensors);
+    timer.setInterval(sendinfo2blynk, sendinfo);
+    timer.setInterval(sendalert2blynk, sendAlert);
+  
+    EEPROM.get(1, lowalertsetpoint);
+    EEPROM.get(50, highalertsetpoint);
+    Blynk.virtualWrite(V20, alertenable);
+    Blynk.virtualWrite(V10, lowalertsetpoint);
+    Blynk.virtualWrite(V11, highalertsetpoint);
+    Blynk.notify(String("Device Rebooting... Allow 1 min for sensor readings to settle")); // +  myStr + ("°F"));
+    pinMode(BlynkAlertLED, OUTPUT);
+    
+    Particle.variable("BME280Ready", BME280Status);
+    Particle.variable("SCD30Ready", SCD30Status);
+    
+    Wire.begin();
+    sensorInit();
+    
+    //Wire.begin();
+    SeeedOled.init();
+    SeeedOled.clearDisplay();
+    SeeedOled.setNormalDisplay();
+    SeeedOled.setPageMode();
+    SeeedOled.setTextXY(2, 0);
+    SeeedOled.putString("Particle");
+    SeeedOled.setTextXY(3, 0);
+    SeeedOled.putString("Air Quality");
+    SeeedOled.setTextXY(4, 0);
+    SeeedOled.putString("Monitor");
+  
+    lastInterval = millis();
 }
 
-void loop()
-{
-    Blynk.run();
+void loop() {
+    if (Blynk.connected()) {  // If connected run as normal
+        Blynk.run();
+    } else if (ReCnctFlag == 0) {
+        ReCnctFlag = 1;  // Set reconnection Flag  
+        //if(debugEnable) { terminal.println("Starting reconnection timer in 30 seconds..."); terminal.flush(); }
+        timer.setTimeout(35000L, []() {  // takes about 30 sec for particle.connected to return false -- blynk.connect will block if particle.connected isn't false yet(setting this lower)
+            ReCnctFlag = 0;   ReCnctCount++;  // Increment reconnection Counter  // Reset reconnection Flag
+            //if (debugEnable) { terminal.print("Attempting reconnection #"); terminal.println(ReCnctCount); }
+            if (Particle.connected()) Blynk.connect();  // Try to reconnect to the server
+        });  // END Timer Function
+    }
+    //Blynk.run();
     timer.run();
     firstrun();
 
-  duration = pulseIn(DUST_SENSOR_PIN, LOW);
-  lowpulseoccupancy = lowpulseoccupancy + duration;
+    duration = pulseIn(DUST_SENSOR_PIN, LOW);
+    lowpulseoccupancy = lowpulseoccupancy + duration;
 
-  if ((millis() - lastInterval) > SENSOR_READING_INTERVAL) {
-    String quality = getAirQuality();
-    //Serial.printlnf("Air Quality: %s", quality.c_str());
-    getBMEValues(temp, pressure, humidity);
-    //Serial.printlnf("Temp: %d", temp);
-    //Serial.printlnf("Pressure: %d", pressure);
-    //Serial.printlnf("Humidity: %d", humidity);
-    getDustSensorReadings();
-    updateDisplay(temp, humidity, pressure, quality);
-    createEventPayload(temp, humidity, pressure, quality);
-    lowpulseoccupancy = 0;
-    lastInterval = millis();
-    
-    if (airSensor.dataAvailable()) { //SCD30 data
-    String SCD_CO2;
-    String SCD_Temp;
-    String SCD_Hum;
-    SCD_CO2 = String(airSensor.getCO2());
-    SCD_Temp = String(airSensor.getTemperature(), 1);
-    SCD_Hum = String(airSensor.getHumidity(), 1);
-    Particle.publish("SCD30/readings", String("CO2:") + SCD_CO2 + (" Temp:") + SCD_Temp + (" Hum:") + SCD_Hum);
-    
-    //Particle.publish("co2(ppm):");
-    //Particle.publish(" temp(C):");
-    //Particle.publish(airSensor.getTemperature(), 1);
-
-    //Particle.publish(" humidity(%):");
-    //Particle.publish(airSensor.getHumidity(), 1);
-  }
-  else Particle.publish("SCD30", "Error in loop()");
-  }
+    /*if ((millis() - lastInterval) > SENSOR_READING_INTERVAL) {
+        digitalWrite(BlynkAlertLED, alertenable);
+        if(alertenable) alertenable = 0;
+        else alertenable = 1;
+        terminal.println(Time.format("%r - %a %D: InLoop()")); terminal.flush();
+        lastInterval = millis();
+    }*/
   
-  if (millis() - lastSync > ONE_DAY_MILLIS) {
+    if (millis() - lastSync > ONE_DAY_MILLIS) {
         Particle.syncTime();
         lastSync = millis();
     }
@@ -209,9 +223,19 @@ void firstrun() {
         firstrunlockout = 1;
     }
 }
+void updateAllSensors() {
+    String quality = getAirQuality();
+    //createEventPayload(temp, humidity, pressure, quality);
+    getBMEValues(temp, pressure, humidity);
+    getSCD30values();
+    getDustSensorReadings(); 
+    lowpulseoccupancy = 0;
+}
 
 void sendinfo() {
       getBMEValues(temp, pressure, humidity); //get current readings
+      String quality = getAirQuality(); //<<< move this out of BME280 stuff
+      if(db2p) createEventPayload(temp, humidity, pressure, quality);
       Blynk.virtualWrite(V0, Time.format("%r - %a %D"));
       Blynk.virtualWrite(V1, temp);
       Blynk.virtualWrite(V2, humidity);
@@ -248,6 +272,20 @@ void resetAlert() {
 /////////************* **********/////////
 //           Sensor Functions           //
 /////////************* **********/////////
+void getSCD30values() {
+    if (airSensor.dataAvailable()) { //SCD30 data
+        String SCD_CO2;
+        String SCD_Temp;
+        String SCD_Hum;
+        SCD_CO2 = String(airSensor.getCO2());
+        int val = (airSensor.getTemperature(), 1);
+        val = val * 1.8F + 32;  //<<<<<<<<<<<<<this isn't right
+        SCD_Temp = String(val);
+        SCD_Hum = String(airSensor.getHumidity(), 1);
+        Particle.publish("SCD30/readings", String(Time.format("%r - %D ")) + ("CO2: ") + SCD_CO2 + (" Temp: ") + SCD_Temp + (" Hum: ") + SCD_Hum, PRIVATE | WITH_ACK);
+    }
+    else Particle.publish("SCD30/readings", "Error in updateAllSensors()");
+}
 String getAirQuality() {
   int quality = aqSensor.slope();
   qual = "None";
@@ -315,10 +353,7 @@ void createEventPayload(int temp, int humidity, int pressure, String airQuality)
       jw.insertKeyValue("dust-concentration", concentration);
     }
   }
-  if(sendtoParticle == 1)
-  {
-      Particle.publish("env-vals", jw.getBuffer(), PRIVATE);
-  }
+  /*if(db2p)*/ Particle.publish("BME280/readings", jw.getBuffer(), PRIVATE);
 
 }
 
